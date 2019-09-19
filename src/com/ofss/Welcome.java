f3e373f909cdb0404113238ecdd87bd236f366d0
Guru <gurumurthy.ramamurthy@gmail.com>
2019-09-19 22:25:16 +0530

    Hello5

--- src/com/ofss/Welcome.java
+++ src/com/ofss/Welcome.java
@@ -3,7 +3,7 @@ package com.ofss;
 public class Welcome {
 
 	public static void main(String[] args) {
-		System.out.println("Hello4");
+		System.out.println("Hello5");
 		System.out.println(Calculator.addition(2, 3));
 
 	}
